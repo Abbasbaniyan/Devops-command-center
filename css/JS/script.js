4cbd73317ecfe8f87c833e53dba8d87cadaa77d3
Abbas Baniyan <abbasbaniyan307@gmail.com>
2026-07-24 13:33:35 +0530

    added live infrastructure

--- css/JS/script.js
+++ css/JS/script.js
@@ -110,3 +110,29 @@ closeBtn.onclick=function(){
 terminal.style.display="none";
 
 }
+function random(min,max){
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+function updateMetrics(){
+
+    const cpu=random(20,75);
+    const ram=random(35,85);
+    const disk=random(25,65);
+    const network=random(50,120);
+
+    cpuBar.style.width=cpu+"%";
+    ramBar.style.width=ram+"%";
+    diskBar.style.width=disk+"%";
+    networkBar.style.width=(network/120*100)+"%";
+
+    cpuValue.innerText=cpu+"%";
+    ramValue.innerText=ram+"%";
+    diskValue.innerText=disk+"%";
+    networkValue.innerText=network+" Mbps";
+
+}
+
+updateMetrics();
+
+setInterval(updateMetrics,3000);
