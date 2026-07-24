0641f2904eb93486a810e528b93095bcc285a53a
Abbas Baniyan <abbasbaniyan307@gmail.com>
2026-07-24 13:39:34 +0530

    uppdated

--- css/JS/script.js
+++ css/JS/script.js
@@ -136,3 +136,25 @@ function updateMetrics(){
 updateMetrics();
 
 setInterval(updateMetrics,3000);
+
+const observer = new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+document.querySelectorAll("section").forEach(section=>{
+
+section.classList.add("hidden");
+
+observer.observe(section);
+
+});
