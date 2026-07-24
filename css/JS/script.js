5019e18ad06ce1c4abd38d76eee3fb31beb7bba9
Abbas Baniyan <abbasbaniyan307@gmail.com>
2026-07-24 13:31:42 +0530

    added interatice devops

--- css/JS/script.js
+++ css/JS/script.js
@@ -34,3 +34,79 @@ function addLog(){
 }
 
 window.onload=addLog;
+
+const terminal=document.getElementById("terminal");
+
+const openBtn=document.getElementById("openTerminal");
+
+const closeBtn=document.getElementById("closeTerminal");
+
+const terminalBody=document.getElementById("terminalBody");
+
+const commands=[
+
+"$ git pull",
+
+"Receiving objects... Done",
+
+"$ docker build -t devops-app .",
+
+"Successfully built image",
+
+"$ terraform apply",
+
+"Infrastructure up-to-date",
+
+"$ ssh ubuntu@server",
+
+"Connected",
+
+"$ sudo systemctl restart nginx",
+
+"Restarted nginx",
+
+"",
+
+"✅ Deployment Successful",
+
+"🌍 Production is LIVE"
+
+];
+
+openBtn.onclick=function(e){
+
+e.preventDefault();
+
+terminal.style.display="flex";
+
+terminalBody.innerHTML="";
+
+let i=0;
+
+function type(){
+
+if(i>=commands.length) return;
+
+const p=document.createElement("p");
+
+p.innerText=commands[i];
+
+terminalBody.appendChild(p);
+
+terminalBody.scrollTop=terminalBody.scrollHeight;
+
+i++;
+
+setTimeout(type,700);
+
+}
+
+type();
+
+}
+
+closeBtn.onclick=function(){
+
+terminal.style.display="none";
+
+}
